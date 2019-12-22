37075833b7ce988c4a9cbe28a354401529a08578
jinxin4u <58205181+jinxin4u@users.noreply.github.com>
2019-12-22 18:38:15 +0900

    12

--- 2_Web_crawling.py
+++ 2_Web_crawling.py
@@ -145,7 +145,26 @@ data3.info()
 data3['종가'] = data3['종가'].astype('int32')
 data3.info()
 
+#%%
+import os
+import sys
+import urllib.request
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
+url = "https://openapi.naver.com/v1/datalab/search";
+body = "{\"startDate\":\"2017-01-01\",\"endDate\":\"2017-04-30\",\"timeUnit\":\"month\",\"keywordGroups\":[{\"groupName\":\"한글\",\"keywords\":[\"한글\",\"korean\"]},{\"groupName\":\"영어\",\"keywords\":[\"영어\",\"english\"]}],\"device\":\"pc\",\"ages\":[\"1\",\"2\"],\"gender\":\"f\"}";
 
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+request.add_header("Content-Type","application/json")
+response = urllib.request.urlopen(request, data=body.encode("utf-8"))
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
 
 
 
